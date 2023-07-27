80a4b197a0daeed93cd2b2ef1613f367b6246f85
anil-22 <anilkumar2214ak@gmail.com>
2023-07-27 12:40:01 +0530

    sample.java file added

---
+++ sample.java
@@ -0,0 +1,43 @@
+
+
+// Sample class to test java code syntax
+
+// import java.lang.Math;
+import java.util.Arrays;
+
+public class sample {
+	sample(){
+
+    }
+    public static void main(String[] args) {
+
+       // sample arr = new sample();
+        // String str = "hi";
+        // // int len=str.length();
+        // System.out.println("length : " + str.length());
+        // System.out.println("abs() : "+Math.abs(-9));
+
+        // int[] arr = {3,4,4,2};
+        // int[] nums=new int[arr.length+1];
+        // nums[arr[0]]++;
+        // System.out.println(Arrays.toString(nums));
+
+        // System.out.println("abu".length());
+
+        // System.out.println('b'-'a');
+        // String str = "abba";
+        boolean[] check = new boolean[2];
+        System.out.println(Arrays.toString(check));
+        // char curChar = str.charAt(0); 
+        // System.out.println(curChar);
+        // System.out.println(check[curChar-'a']);
+
+        // System.out.println("Strind".substring(1));
+
+        // System.out.println(((int)(Math.log10(Math.abs(-870)))+1));
+
+        int[][] arr ={};
+        System.out.println(arr.length);
+
+    }
+}
